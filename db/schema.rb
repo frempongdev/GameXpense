8e9da8ffc810b47121ed8b5bc617e1f1a3edd179
Frempong <samankwaa.frempong@gmail.com>
2023-06-15 04:51:54 +0000

    Add Expense to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_024028) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_030410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_024028) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "expenses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "name"
+    t.decimal "amount"
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_expenses_on_game_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
+
   create_table "games", force: :cascade do |t|
     t.text "name"
     t.bigint "user_id", null: false
@@ -65,5 +76,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_024028) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "expenses", "games"
+  add_foreign_key "expenses", "users"
   add_foreign_key "games", "users"
 end
