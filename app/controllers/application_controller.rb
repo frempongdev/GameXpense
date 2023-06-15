d5c2897ce6dbd9191fa77d1a0d49b73f95c09547
Frempong <samankwaa.frempong@gmail.com>
2023-06-15 16:11:50 +0000

    Fix Linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :profile_photo) }
   end
 
-  private 
+  private
+
   def after_sign_in_path_for(resource)
     # Customize the redirect path based on your requirements
     # For example:
